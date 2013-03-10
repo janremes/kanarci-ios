df076a2d9b6675d20432469caa2724fba519f515
Jan Remes <remeja@gmail.com>
2013-03-10 22:47:29 +0100

    Initial Commit

---
+++ kanarci/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kanarci
+//
+//  Created by Jan Remes on 10.03.13.
+//  Copyright (c) 2013 Jan Remes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kanarci/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  kanarci
+//
+//  Created by Jan Remes on 10.03.13.
+//  Copyright (c) 2013 Jan Remes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
